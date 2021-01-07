2df6b8a1bffcfeb1b775504c7f4dd385e844f727
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-07 15:31:01 -0300

    Primeira aula pratica do curso

---
+++ scripts-python/aula-04-desafios/desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia do seu aniversário? ')
+mes = input('Qual o mês do seu aniversário? ')
+ano = input('Qual ano você nasceu? ')
+
+print('Data de nascimento:', dia + '/' + mes + '/' + ano)
---
+++ scripts-python/aula-04-desafios/desafio03.py
@@ -0,0 +1,4 @@
+num1 = int(input('Informe um número: '))
+num2 = int(input('Informe outro número: '))
+
+print('A soma entre', num1, 'e', num2, 'é:', num1 + num2) 
