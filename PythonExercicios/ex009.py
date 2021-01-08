25eef3e3e68abe88713c70e61cbe3f58c2827217
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-08 19:12:53 -0300

    Ajuste

--- PythonExercicios/ex009.py
+++ PythonExercicios/ex009.py
@@ -1,5 +1,10 @@
 numero = int(input('Informe um número inteiro: '))
 
 print('A tabuada de {} é: '.format(numero))
+
+print('-' * 11)
+
 for i in range(1, 11, 1):
-    print('{} x {} = {}'.format(numero, i, numero * i))
+    print('{} x {:2} = {}'.format(numero, i, numero * i))
+
+print('-' * 11)
