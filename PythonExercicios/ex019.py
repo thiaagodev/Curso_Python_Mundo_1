220859559ddba2f3991ff6fd2fac0e4ea508d57f
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-10 17:39:22 -0300

    Teste

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -5,6 +5,8 @@ aluno2 = input('Digite o nome do aluno número 2: ')
 aluno3 = input('Digite o nome do aluno número 3: ')
 aluno4 = input('Digite o nome do aluno número 4: ')
 
-aluno_escolhido = random.choice([aluno1, aluno2, aluno3, aluno4])
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+aluno_escolhido = random.choice(lista)
 
 print('O aluno escolhido foi o {}'.format(aluno_escolhido))
